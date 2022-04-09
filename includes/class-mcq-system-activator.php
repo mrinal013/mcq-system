5895c15dba62e2972e23c570fa981ba7bd942bf9
Mrinal Haque <mrinalhaque99@gmail.com>
2022-04-09 13:28:38 +0600

    post type init

---
+++ includes/class-mcq-system-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://mrinalbd.com/
+ * @since      1.0.0
+ *
+ * @package    Mcq_System
+ * @subpackage Mcq_System/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Mcq_System
+ * @subpackage Mcq_System/includes
+ * @author     Mrinal Haque <mrinalhaque99@gmail.com>
+ */
+class Mcq_System_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
